15133b60b6307d7fcf1c8195372ec5fe27579f56
kyriosdata <fabio@inf.ufg.br>
2022-12-21 12:15:28 -0300

    documentação melhorada

--- poc/src/main/java/com/github/kyriosdata/docenteinf/domain/Avaliacao.java
+++ poc/src/main/java/com/github/kyriosdata/docenteinf/domain/Avaliacao.java
@@ -6,13 +6,30 @@ import lombok.Setter;
 import java.time.LocalDate;
 
 /**
- * Representa uma avaliação.
+ * Uma avaliação de um docente.
  */
 @Getter
 @Setter
 public class Avaliacao {
+
+    /**
+     * O docente cujos dados são empregados
+     * na avaliação.
+     */
     private Docente docente;
+
+    /**
+     * A data em que foi requisitada a avaliação.
+     */
     private LocalDate data;
+
+    /**
+     * A identificação de quem fez a requisição.
+     */
     private String requisitadaPor;
+
+    /**
+     * A análise produzida pela avaliação.
+     */
     private Analise analise;
 }
